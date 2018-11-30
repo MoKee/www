24ba7f8b96097353c0d34a2eca89da52143e30df
martincz <martincz@mokeedev.com>
2018-12-01 06:31:28 +0800

    update guide page

--- www/.vuepress/config.js
+++ www/.vuepress/config.js
@@ -2,6 +2,8 @@ module.exports = {
   themeConfig: {
     search: false,
     lastUpdated: 'Last Updated',
+    repo: 'MoKee/www',
+    editLinks: true,
     locales: {
       '/': {
         selectText: 'Languages',
@@ -10,16 +12,19 @@ module.exports = {
           { text: 'Home', link: '/' },
           { text: 'Download', link: 'https://download.mokeedev.com' },
           { text: 'Forum', link: 'https://bbs.mokeedev.com' },
-        ]
+        ],
+        editLinkText: 'Edit this page on GitHub'
       },
       '/zh/': {
         selectText: '选择语言',
         label: '简体中文',
         nav: [
           { text: '主页', link: '/zh/' },
+          { text: '教程', link: '/zh/guide/' },
           { text: '下载', link: 'https://download.mokeedev.com' },
           { text: '论坛', link: 'https://bbs.mokeedev.com' },
-        ]
+        ],
+        editLinkText: '在 GitHub 上编辑此页'
       }
     }
   },
